80ab3c9349a731796da661f069c3dea9afc9926a
OM00n <owenmoon2310@gmail.com>
2024-11-28 12:30:26 +0100

    improved error handling and added closing

--- project/database/create tables/sql_functions.py
+++ project/database/create tables/sql_functions.py
@@ -1,34 +1,43 @@
 import pymysql
-import json
+import sys
+import pwinput
+import pymysql.cursors
+from pymysql.err import MySQLError
 
 def get_db_information() -> tuple[str, str] | None:
     try:
         database = str(input("Enter the name of your MySQL database:\n"))
-        passwd = str(input("Enter the password for your MySQL:\n"))
-    except:
-        print("Invalid input.")
-        return 
-    return database, passwd
+        password = str(pwinput.pwinput("Enter the password for your MySQL:\n"))
+        return database, password
+    except Exception as e:
+        sys.exit(f"Invalid input : {e}.")
+
     
-def create_connection(database: str, passwd: str) -> pymysql.connections.Connection:
+def create_connection(database: str, password: str) -> pymysql.connections.Connection | None:
     try:
         connection = pymysql.connect(
             host="localhost",
             user="root",
-            password=passwd,
+            password=password,
             database=database)
-    except:
-        print("Connection failed.")
-        return
-    return connection
+        return connection        
+    except MySQLError as e:
+        sys.exit(f"Database connection failed: {e}.")
 
-def create_cursor(connection: pymysql.connections.Connection) -> pymysql.cursors.Cursor:
+
+def create_cursor(connection: pymysql.connections.Connection) -> pymysql.cursors.Cursor | None:
     try:
         cursor = connection.cursor()
-    except:
-        print("Cursor creation failed.")
-        return
-    return cursor
+        return cursor
+    except MySQLError as e:
+        sys,exit(f"Error creating cursor: {e}.")
+
+def close_cursor_and_connection(cursor: pymysql.cursors.Cursor, connection: pymysql.connections.Connection) -> None:
+    try:
+        cursor.close()
+        connection.close()
+    except MySQLError as e:
+        sys.exit(f"Error closing connection: {e}.")
 
 
 if __name__ == "__main__":
@@ -39,3 +48,4 @@ if __name__ == "__main__":
                 id INT,
                 age INT
                 )""")
+    close_cursor_and_connection(cursor, connection)
