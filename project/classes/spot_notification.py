dbab17104f8133666ff2e6000b5ff7c0803cc48a
yirizwart <118004591+yirizwart@users.noreply.github.com>
2024-11-29 14:27:54 +0100

    dataclasses: Spot and Notification

---
+++ project/classes/spot_notification.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+class Plant:
+    pass
+
+@dataclass
+class Spot:
+    '''Class for setting the attributes for a spot created in a room
+    with the properties lightlevel, humidity, temperature and 
+    setting a plant to a spot.'''
+    spot_id: str
+    ligthlevel: str
+    humidity: str
+    assigned_plant: Plant | None
+    temperature: int
+
+@dataclass
+class Notification:
+    '''Class that stores a notification together with 
+    the importance(weight), the type and the time 
+    of the notification for a certain plant.
+    '''
+    weight: str
+    time_sent: datetime
+    name: str
+    type: str
+    plant_notification: Plant
+
+
+
+
