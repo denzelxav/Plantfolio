164cf68dfeeb7b41a06b873e26c1a2b341e26c84
LAPTOP_DENZEL\denze <denzeltraeger@hotmail.com>
2024-12-06 10:57:11 +0100

    Fixed typeError in delete_spot method

--- project/classes/userdata.py
+++ project/classes/userdata.py
@@ -47,6 +47,6 @@ class UserData:
     def delete_spot(self, bad_spot: Spot) -> None:
         '''Removes a spot, but only if it is empty'''
         if bad_spot.assigned_plant is None:
-            for room in self.rooms.items():
-                if bad_spot in self.rooms[room]:
+            for room, spots in self.rooms.items():
+                if bad_spot in spots:
                     self.rooms[room].remove(bad_spot)
