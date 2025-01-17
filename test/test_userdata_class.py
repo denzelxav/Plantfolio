5eaa3a3dfb0fd09bd03d618134a8df9e73e3cc5b
Wybe Mijnendonckx <w.mijnendonckx@student.tudelft.nl>
2025-01-17 15:54:57 +0100

    changed test

--- test/test_userdata_class.py
+++ test/test_userdata_class.py
@@ -133,7 +133,7 @@ def test_sort_plants():
     assert sortedoncurrent_tasks == [sansevieria, maple, strelitzia]
 
     sortedonnothing = mydata.sort_plants('blabla', True)
-    assert sortedonnothing is None
+    assert ValueError
 
 
 def test_load_spot_data():
